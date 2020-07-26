e006c7157e18ffe21d164589817665f5d4963f9d
Chelsea Hilditch <git@cdhildit.ch>
2020-07-26 13:49:59 +0100

    Move encode function to Hamming class

--- src/correcter/Main.java
+++ src/correcter/Main.java
@@ -120,41 +120,6 @@ public class Main {
         }
     }
 
-    private static byte[] encode(byte[] bytes) {
-        // Implement Hamming code [7,4]
-        int numberOfBits = bytes.length * 8;
-        byte[] encoded = new byte[bytes.length * 2];
-
-        int[] currentBits;
-
-        int byteNo, bitNo;
-
-        for (int bitCounter = 0; bitCounter < numberOfBits; bitCounter += 4) {
-            currentBits = new int[]{0, 0, 0, 0};
-            for (int i = 0; i < 4; i++) {
-                byteNo = (bitCounter + i) / 8;
-                bitNo = (bitCounter + i) % 8;
-
-                if (byteNo < bytes.length) {
-                    currentBits[i] = (bytes[byteNo] >> (7 - bitNo)) & 1;
-                }
-            }
-
-            // Calculate parities
-            // parity = 1 => odd number of 1s in input
-            // parity = 0 => even number of 1s in input
-            int parity1 = calculateParity(currentBits[0], currentBits[1], currentBits[3]);
-            int parity2 = calculateParity(currentBits[0], currentBits[2], currentBits[3]);
-            int parity3 = calculateParity(currentBits[1], currentBits[2], currentBits[3]);
-
-            int newByte = (parity1 << 7 | parity2 << 6 | currentBits[0] << 5 | parity3 << 4
-                    | currentBits[1] << 3 | currentBits[2] << 2 | currentBits[3] << 1);
-
-            encoded[bitCounter / 4] = (byte) newByte;
-        }
-        return encoded;
-    }
-
     private static byte[] correct(byte[] bytes) {
         // Correct the error in each byte
         byte[] corrected = new byte[bytes.length];
